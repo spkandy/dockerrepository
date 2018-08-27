fefc2cd106d6af62d56f297b85a54a02737ba440
root <root@ip-172-31-14-185.ap-southeast-1.compute.internal>
2018-08-27 11:11:28 +0000

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+   public static void main(String[] args) {
+      // Prints "Hello, World" in the terminal window.
+      System.out.println("Hello, World");
+      System.out.println("Qick Change");
+      System.out.println("Anil bidari");
+   }
+}
